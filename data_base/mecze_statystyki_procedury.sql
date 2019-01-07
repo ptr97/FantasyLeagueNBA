dc772b891a3209283d17e1c09f48874a7e4b22b0
Piotr <ptr97@users.noreply.github.com>
2019-01-07 16:41:21 +0100

    Add games and stats triggers to DB

--- data_base/mecze_statystyki_procedury.sql
+++ data_base/mecze_statystyki_procedury.sql
@@ -19,3 +19,28 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER _01_aktualizujZarobkiZawodnikow 
     AFTER INSERT ON nba.statystyki_meczu
     FOR EACH ROW EXECUTE PROCEDURE nba.aktualizujZarobkiZawodnikow();
+
+-- Procedury aktualizujace wyniki druzyn zawodnikow po wstawieniu nowych statystyk z meczow
+
+CREATE FUNCTION nba.aktualizujWynikiZespolow() RETURNS TRIGGER AS $$
+    DECLARE
+        zdobycze_zawodnika int;
+        zawodnik_zespol nba.zawodnicy_zespoly_uzytkownikow%ROWTYPE;
+        stary_wynik_druzyny int;
+        nowy_wynik_druzyny int;
+    BEGIN
+        zdobycze_zawodnika := NEW.punkty_zawodnika + NEW.asysty_zawodnika + NEW.zbiorki_zawodnika + NEW.przechwyty_zawodnika - NEW.straty_zawodnika;
+
+        FOR zawodnik_zespol IN SELECT * FROM nba.zawodnicy_zespoly_uzytkownikow WHERE id_zawodnika = NEW.id_zawodnika LOOP
+            SELECT INTO stary_wynik_druzyny wynik_zespolu_uzytkownika FROM nba.zespoly_uzytkownikow WHERE id_zespolu_uzytkownika = zawodnik_zespol.id_zespolu_uzytkownika;
+            nowy_wynik_druzyny := stary_wynik_druzyny + zdobycze_zawodnika;
+            UPDATE nba.zespoly_uzytkownikow SET wynik_zespolu_uzytkownika = nowy_wynik_druzyny WHERE id_zespolu_uzytkownika = zawodnik_zespol.id_zespolu_uzytkownika;
+        END LOOP;
+
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER _02_aktualizujWynikiZespolow
+    AFTER INSERT ON nba.statystyki_meczu
+    FOR EACH ROW EXECUTE PROCEDURE nba.aktualizujWynikiZespolow();
